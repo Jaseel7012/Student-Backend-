487688102db61e99a91907a7991cc9ff10add673
jaseel <jaseeljctcs@gmail.com>
2022-12-08 21:49:50 +0530

    search

--- src/main/java/com/example/StudentBackend/Controller/StudentsController.java
+++ src/main/java/com/example/StudentBackend/Controller/StudentsController.java
@@ -3,10 +3,7 @@ package com.example.StudentBackend.Controller;
 import com.example.StudentBackend.Dao.StudentsDao;
 import com.example.StudentBackend.Model.StudentsModel;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -14,19 +11,26 @@ import java.util.List;
 public class StudentsController {
     @Autowired
     private StudentsDao dao;
-
+    @CrossOrigin(origins = "*")
     @PostMapping(path="/add",consumes = "application/json",produces = "application/json")
 
     public String addstu(@RequestBody StudentsModel s){
         dao.save(s);
         return "add";
     }
+    @CrossOrigin(origins = "*")
     @GetMapping("/view")
     public List<StudentsModel> view(){
         return (List<StudentsModel>) dao.findAll();
     }
-    @PostMapping("/search")
-    public String search(){
-        return "search";
+    @CrossOrigin(origins = "*")
+    @PostMapping(path="/search",consumes = "application/json",produces = "application/json")
+    public List<StudentsModel> search(@RequestBody StudentsModel s){
+        String roll_no=String.valueOf(s.getRollno());
+        System.out.println(roll_no);
+        return  ( List<StudentsModel>) dao.Searchstudent(s.getRollno());
+
+
+
     }
 }
--- src/main/java/com/example/StudentBackend/Dao/StudentsDao.java
+++ src/main/java/com/example/StudentBackend/Dao/StudentsDao.java
@@ -1,7 +1,14 @@
 package com.example.StudentBackend.Dao;
 
 import com.example.StudentBackend.Model.StudentsModel;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface StudentsDao extends CrudRepository<StudentsModel,Integer> {
+
+    @Query(value = "SELECT `id`, `clg`, `name`, `rollno` FROM `student` WHERE `rollno`= :rollno",nativeQuery = true)
+    List<StudentsModel> Searchstudent(@Param("rollno") Integer rollno);
 }
